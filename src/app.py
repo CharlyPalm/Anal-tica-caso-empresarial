f0a4fe85170450deeeabdef72325d74398c335f8
CharlyPalm <palmiericarlos@outlook.com>
2024-06-11 22:16:57 -0600

    Adding app for streamlit and editing ipynb

---
+++ src/app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import pydeck as pdk
+
+st.title("Hola Carlos Mundo")
+st.header("Esta es mi clase favorita \U0001F910 \U0001F52B")
+
+
+
